d9f1295a87b5572b1a4d7c46c843538d4d59f76c
commaeng <commaeng09@gmail.com>
2019-06-25 22:22:10 +0900

    selector

--- tubular/js/index.js
+++ tubular/js/index.js
@@ -1,6 +1,4 @@
 $('document').ready(function() {
-	var options = { videoId: 'e4Is32W-ppk', start: 3 };
+	var options = { videoId: 'jQHJYNTMymQ', start: 3 };
 	$('#wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
 });
